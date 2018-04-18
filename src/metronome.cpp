0335b56b378d89f0bb62c0276fa9bee751527bcb
jsteele13 <jenni1317@gmail.com>
2018-04-17 20:52:15 -0500

    used std::chronoworks for 60bpm

--- src/metronome.cpp
+++ src/metronome.cpp
@@ -1,23 +1,24 @@
 #include "metronome.h"
 
 // http://openframeworks.cc/learning/04_sound/play_a_sound/
-// http://openframeworks.cc/documentation/utils/ofTimer/
+// http://www.cplusplus.com/reference/chrono/steady_clock/
 
 Metronome::Metronome(int tempo) {
+	using namespace std::chrono;
 	myMetronome.load(CLICK_SOUND_FILENAME);
+	double waitTime = 60 / tempo; //time durration in milliseconds
 
-	int timeNano = (6 * pow(10, 10)) / tempo; //time in nanoseconds
-	t.setPeriodicEvent(timeNano); 
-	startThread();
-}
-
-void Metronome::startTicking() {
-	while (isThreadRunning()) {
+	while (true) {
+		steady_clock::time_point t1 = steady_clock::now();
 		myMetronome.play();
-		t.waitNext();
+		bool flag = false;
+		while (!flag) {
+			steady_clock::time_point t2 = steady_clock::now();
+			if (duration_cast<duration<double>>(t2 - t1).count() >= waitTime) {
+				flag = true;
+			}
+		}
 	}
 }
 
-void Metronome::stopTicking() {
-	stopThread();
-}
+
--- src/metronome.h
+++ src/metronome.h
@@ -1,20 +1,15 @@
 #pragma once
-#include "ofThread.h"
-#include "ofTimer.h"
+#include <chrono>
 #include "ofSoundPlayer.h"
 
 #define CLICK_SOUND_FILENAME "short_metronome_click.wav"
 #define LONG_SOUND_FILENAME "metronome_click.wav"
 #define CLICK_LENGTH_NS 132000000
 
-class Metronome : public ofThread {
+class Metronome {
 	public: 
 		Metronome(int tempo); // tempo in bpm
 
 	private:
-		ofTimer t;
 		ofSoundPlayer myMetronome;
-
-		void startTicking();
-		void stopTicking();
 };
